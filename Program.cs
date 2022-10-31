d8ab3441caa53c4dcb2f77e615bc681b8a55e595
Onur Arif Ciftci <101008431+OnurArif1@users.noreply.github.com>
2022-10-31 18:57:33 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+bool HasCycle(Node head)
+{
+    Node temp = head;
+    Node temp2 = head;
+
+    while (temp2 != null && temp2.next != null)
+    {
+        temp = temp.next;
+        temp2 = temp2.next.next;
+
+        if (temp == temp2)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+class Node
+{
+    Node head;
+    public Node next;
+
+    public Node(Node head)
+    {
+        this.head = head;
+        this.next = null;
+    }
+}
